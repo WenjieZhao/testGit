9467200d573aa122ad33d292b498c73b10f0ac63
WenjieZhao <zhao_wenjie@hotmail.com>
2017-01-10 15:55:01 +0800

    2.0 version

--- src/java_firstclass/Hello.java
+++ src/java_firstclass/Hello.java
@@ -5,7 +5,8 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
-		System.out.println("123555");
+		System.out.println("12355");
+		System.out.println("1235");
 	}
 
 }
